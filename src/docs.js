b1d0b626da8ccc41c4bfe7048146fbcca8191342
nicolasm77 <40019132+nicolasm77@users.noreply.github.com>
2018-08-22 14:06:34 +0200

    test jquery

--- src/docs.js
+++ src/docs.js
@@ -1,10 +1,10 @@
 import $ from "jquery";
-window.$j = $.noConflict();
+// window.$j = $.noConflict();
 
-import bubbles from "./index.js";
+// import bubbles from "./index.js";
 import bubblesCSS from "./sass/bubble.scss";
 
-import anims from "./anims.js";
+// import anims from "./anims.js";
 
 
 import docCSS from "./doc/sass/demo.scss";
